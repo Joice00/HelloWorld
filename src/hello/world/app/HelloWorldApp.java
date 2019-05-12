70d5d28229a39eaa933b8c51972909e961373d33
jossiana <jossiana.moniz.00@gmail.com>
2019-05-12 17:39:21 +0100

    segundo commint

--- src/hello/world/app/HelloWorldApp.java
+++ src/hello/world/app/HelloWorldApp.java
@@ -16,7 +16,7 @@ public class HelloWorldApp {
      */
     public static void main(String[] args) {
         
-    System.out.println ("Hello World!");
+    System.out.println ("ola mundo!");
     }
     
 }
